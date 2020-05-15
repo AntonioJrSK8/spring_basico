526a33d1d9a7c3b1dae92957006d152a8ccd39c0
Antonio Junior <jr.sistema@gmail.com>
2020-05-15 18:10:38 -0300

    configuração de path dinamico

--- src/main/java/com/java_spring/java_spring/controller/primeiroController.java
+++ src/main/java/com/java_spring/java_spring/controller/primeiroController.java
@@ -1,6 +1,7 @@
 package com.java_spring.java_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,11 @@ public class primeiroController {
     public String subPath(){
         return "sub path.";
     }
+
+    @GetMapping("/{dynamic}")
+    public String dynamico(@PathVariable("dynamic") String name) {
+        return "Nome do parametro " + name;
+    }
+
+    
 }
