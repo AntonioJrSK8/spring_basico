41900a47d21b8788d3ae10c6374b96e59cd6799a
Antonio Junior <jr.sistema@gmail.com>
2020-05-15 18:56:21 -0300

    alteração de status de code retorno

--- src/main/java/com/java_spring/java_spring/controller/primeiroController.java
+++ src/main/java/com/java_spring/java_spring/controller/primeiroController.java
@@ -1,10 +1,12 @@
 package com.java_spring.java_spring.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
@@ -27,6 +29,7 @@ public class primeiroController {
     // http://localhost:8080/subpath?name=Pedro
     @GetMapping("/subpath2")
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.OK)
     public String subPathQueryString(@RequestParam(value = "name", required = false) String name) {
         return "Valor de uma Query String: " + name;
     }
