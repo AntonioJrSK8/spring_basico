2fb87f1d8ae88a12e37432d84c40a03a71f3b9fd
Antonio Junior <jr.sistema@gmail.com>
2020-05-15 18:16:37 -0300

    configuração de query string

--- src/main/java/com/java_spring/java_spring/controller/primeiroController.java
+++ src/main/java/com/java_spring/java_spring/controller/primeiroController.java
@@ -3,6 +3,7 @@ package com.java_spring.java_spring.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,11 +21,17 @@ public class primeiroController {
     public String subPath(){
         return "sub path.";
     }
+    
+    // http://localhost:8080/subpath?name=Pedro
+    @GetMapping("/subpath")
+    public String subPathQueryString(@RequestParam("name") String name) {
+        return "Valor de uma Query String: " + name;
+    }
 
     @GetMapping("/{dynamic}")
     public String dynamico(@PathVariable("dynamic") String name) {
         return "Nome do parametro " + name;
     }
 
-    
+
 }
