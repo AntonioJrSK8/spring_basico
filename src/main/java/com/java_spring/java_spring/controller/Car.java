c923ea1df86f49d793582d9479872110e6428ad0
Antonio Junior <jr.sistema@gmail.com>
2020-05-17 11:51:06 -0300

    configuração e criação de tabela no banco de dados mysql

---
+++ src/main/java/com/java_spring/java_spring/controller/Car.java
@@ -0,0 +1,61 @@
+package com.java_spring.java_spring.controller;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Car
+ */
+@Entity
+@Table(name = "cars")
+public class Car {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer ano;
+    
+    @Column
+    private String cor;
+
+    public Car(String name, Integer ano, String cor) {
+        this.name = name;
+        this.ano = ano;
+        this.cor = cor;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
