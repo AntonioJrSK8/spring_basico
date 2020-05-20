aba072ad0ba1ce6efecb4b69341bbacd6e86b882
Antonio Junior <jr.sistema@gmail.com>
2020-05-19 22:13:32 -0300

    Deletando e atualizando dados cadastrados

--- src/main/java/com/java_spring/java_spring/controller/CarController.java
+++ src/main/java/com/java_spring/java_spring/controller/CarController.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -40,8 +43,15 @@ public class CarController {
 
     @PostMapping
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.CREATED)
     public Car create(@RequestBody Car car){
         return this.carRepository.save(car);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void removeById(@PathVariable("id") Long id) {
+        this.carRepository.deleteById(id);
     }
     
 }
